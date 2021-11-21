99165bb9fc9dd19914e16c8799f31e6ee47aed29
PabloMerener <pablo.merener@gmail.com>
2021-11-21 18:38:21 -0300

    mock_recipes_endpoint

---
+++ api/src/routes/recipes.js
@@ -0,0 +1,11 @@
+'use strict';
+var path = require('path');
+
+const express = require('express');
+const router = express.Router();
+
+router.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../../mock_api/detailed_recipes.json'));
+});
+
+module.exports = router;
