b73bac2107ca901f13813fe56042c205210a3c33
PabloMerener <pablo.merener@gmail.com>
2021-11-21 12:13:34 -0300

    home_button

--- client/src/components/LandingPage.js
+++ client/src/components/LandingPage.js
@@ -1,9 +1,13 @@
 import React from "react";
+import { useNavigate } from 'react-router-dom';
 
 const LandingPage = () => {
+    const navigate = useNavigate();
+
     return (
         <div>
             <h1>Henry Food</h1>
+            <button onClick={() => {navigate('/home')}} type="button">Home</button>
         </div>
     );
 }
