d6443e438939258f5026854fb7a7149a3b3d8929
PabloMerener <pablo.merener@gmail.com>
2021-11-24 23:48:27 -0300

    real_api

--- api/src/routes/recipes.js
+++ api/src/routes/recipes.js
@@ -6,16 +6,44 @@ const router = express.Router();
 
 const fs = require('fs');
 
-router.get('/', (req, res) => {
-    const filePath = path.join(__dirname, '../../mock_api/detailed_recipes.json');
-    const data = JSON.parse(fs.readFileSync(filePath, 'utf8'));
+require('dotenv').config();
+const {
+    API, API_KEY, MOCK_API
+} = process.env;
 
+const https = require('https')
+
+const setFilterToResponse = (req, res, data) => {
     const { name } = req.query;
+    const json = JSON.parse(data);
 
     if (name) {
-        res.send(data.results.filter(e => e.title.toUpperCase().indexOf(name.toUpperCase()) !== -1));
+        res.send(json.results.filter(e => e.title.toUpperCase().indexOf(name.toUpperCase()) !== -1));
     } else {
-        res.send(data.results);
+        res.send(json.results);
+    }
+}
+
+router.get('/', (req, res) => {
+    if (MOCK_API === 'true') {
+        const filePath = path.join(__dirname, '../../mock_api/detailed_recipes.json');
+        setFilterToResponse(req, res, fs.readFileSync(filePath, 'utf8'));
+    } else {
+        const url = API + '/recipes/complexSearch?addRecipeInformation=true&apiKey=' + API_KEY;
+
+        https.get(url, function (httpsRes) {
+            var body = '';
+
+            httpsRes.on('data', function (chunk) {
+                body += chunk;
+            });
+
+            httpsRes.on('end', function () {
+                setFilterToResponse(req, res, body);
+            });
+        }).on('error', function (e) {
+            console.log("Got an error: ", e);
+        });
     }
 });
 
