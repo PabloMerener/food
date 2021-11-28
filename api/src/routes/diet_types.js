806d36883cd40d696859a31e96ff91d4e66b5ca3
PabloMerener <pablo.merener@gmail.com>
2021-11-28 11:18:42 -0300

    set_recipes_diets_relationship

--- api/src/routes/diet_types.js
+++ api/src/routes/diet_types.js
@@ -5,23 +5,20 @@ const fs = require('fs');
 const path = require('path');
 const { Diet } = require('../db.js');
 
-router.get('/', (req, res) => {
-    Diet.count().then(e => {
-        if (e) {
-            Diet.findAll().then(e => res.send(e));
-        } else {
-            const filePath = path.join(__dirname, '../../mock_api/diet_types.json');
-            const json = JSON.parse(fs.readFileSync(filePath, 'utf8'));
+router.get('/', async (req, res) => {
+    const results = await Diet.findAll();
 
-            for (let index = 0; index < json.results.length; index++) {
-                const name = json.results[index].name;
-                Diet.findOrCreate({ where: { name } })
-                    .then(e => console.log([e[0].toJSON(), e[1]]));
-            }
-
-            res.send(json.results);    
+    if (results.length) {
+        res.send(results);
+    } else {
+        const filePath = path.join(__dirname, '../../mock_api/diet_types.json');
+        const json = JSON.parse(fs.readFileSync(filePath, 'utf8'));
+        for (let index = 0; index < json.results.length; index++) {
+            const name = json.results[index].name;
+            Diet.create({ name });
         }
-    });
+        res.send(json.results);
+    }
 });
 
 module.exports = router;
